a675738da811b5b40dea34bc86549a53464722e8
Pauras Swami <pauras53@yahoo.com>
2018-08-11 15:26:33 -0700

    initiate Navbar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import logo from '../../images/logo.svg';
+
+import Navbar from 'components/Navbar';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Test</h1>
-        </header>
+        <Navbar />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <header>
+      Header
+    </header>
+  );
+};
+
+export default Navbar;
