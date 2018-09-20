f07575308cdb5ad6363593d2f59c135a808b2d24
Pauras Swami <pauras53@yahoo.com>
2018-09-19 23:06:57 -0700

    update

--- config/paths.js
+++ config/paths.js
@@ -34,7 +34,7 @@ const getPublicUrl = appPackageJson =>
 function getServedPath(appPackageJson) {
   const publicUrl = getPublicUrl(appPackageJson);
   const servedUrl =
-    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/');
+    envPublicUrl || (publicUrl ? url.parse(publicUrl).pathname : '/swamiayurveda');
   return ensureSlash(servedUrl, true);
 }
 
