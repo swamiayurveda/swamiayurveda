44d17bb0735af000f6911c132992f12d6f922ce5
Pauras Swami <pauras53@yahoo.com>
2019-04-18 21:57:05 -0700

    feat: add overview

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -4,6 +4,9 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+import image from '../../images/plants.png'
 
 import Browse from 'routes/Browse';
 
@@ -32,6 +35,75 @@ const navbarHeight = 52;
       // </div>
     // </div>
   // </section>
+
+const Overview = () => {
+  return(
+    <Container>
+      <Row className="mt-5">
+        <Col>
+        </Col>
+        <Col>
+          <h3 class="text-left mb-5">What is Ayurveda?</h3>
+          <p class="text-left">Bloom is an end-to-end protocol for identity attestation, risk assessment, and credit scoring; entirely on the blockchain. Bloom allows both traditional and digital currency lenders to serve billions of people who currently cannot obtain a bank account or credit score.</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+const StatusQuo = () => {
+  return(
+    <Container>
+      <Row>
+        <Col xs={12} md={6} lg={4} className="mb-5">
+          <Card className="text-center">
+            <Card.Body>
+              <div class="text-center mb-4">
+                <Image src="http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png" roundedCircle />
+              </div>
+              <Card.Title>
+                <h2>Vata</h2>
+              </Card.Title>
+              <Card.Text class="mt-4">
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} md={6} lg={4} className="mb-5">
+          <Card className="text-center">
+            <Card.Body>
+              <div class="text-center mb-4">
+                <Image src="http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png" roundedCircle />
+              </div>
+              <Card.Title>
+                <h2>Kapha</h2>
+              </Card.Title>
+              <Card.Text class="mt-4">
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col xs={12} md={6} lg={4} className="mb-5">
+          <Card className="text-center">
+            <Card.Body>
+              <div class="text-center mb-4">
+                <Image src="http://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png" roundedCircle />
+              </div>
+              <Card.Title>
+                <h2>Pitta</h2>
+              </Card.Title>
+              <Card.Text class="mt-4">
+                With supporting text below as a natural lead-in to additional content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 class Home extends React.Component {
   render() {
@@ -69,6 +141,8 @@ class Home extends React.Component {
               </Col>
             </Row>
           </Container>
+          <Overview />
+          <StatusQuo />
         </div>
       </Container>
     );
