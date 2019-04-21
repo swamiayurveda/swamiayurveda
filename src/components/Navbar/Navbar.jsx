ffe8a9665f9d9498362346729959510d89fc0d6b
Pauras Swami <pauras53@yahoo.com>
2019-04-21 14:11:12 -0700

    feat: change padding on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,49 +22,46 @@ class Navbarr extends React.Component {
 
   render() {
     return (
-      <Navbar bg="white" expand="lg">
-        <Container className="mt-3">
-          <Navbar.Brand className="navbar-item" as={Link} to="/">
-            <img className="navbar-logo" src={image} alt="logo" />
-            <span className="title ml-3">Swami Ayurveda</span>
-          </Navbar.Brand>
-          <Navbar.Toggle className="rounded-pill" aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              {navItems.map(item => (
-                <Nav.Item key={item.to}>
-                  <Link className="nav-link" to={item.to}>
-                    {/* <img className="mr-2" style={{ height: 20 }} src={item.image} alt="logo" /> */}
-                    <span>{item.title}</span>
-                  </Link>
+      <Navbar className="app-navbar" bg="white" expand="lg">
+        <Navbar.Brand className="navbar-item" as={Link} to="/">
+          <img className="navbar-logo" src={image} alt="logo" />
+          <span className="title ml-3">Swami Ayurveda</span>
+        </Navbar.Brand>
+        <Navbar.Toggle className="rounded-pill" aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            {navItems.map(item => (
+              <Nav.Item key={item.to}>
+                <Link className="nav-link" to={item.to}>
+                  {/* <img className="mr-2" style={{ height: 20 }} src={item.image} alt="logo" /> */}
+                  <span>{item.title}</span>
+                </Link>
+              </Nav.Item>
+            ))}
+            <Dropdown className="nav-avatar-dropdown">
+              <Dropdown.Toggle class="nav-avatar-toggle" as="div">
+                <Nav.Item className="navbar-avatar">
+                  <i class="far fa-user text-success"></i>
                 </Nav.Item>
-              ))}
-              <Dropdown className="nav-avatar-dropdown">
-                <Dropdown.Toggle class="nav-avatar-toggle" as="div">
-                  <Nav.Item className="navbar-avatar">
-                    <i class="far fa-user text-success"></i>
-                  </Nav.Item>
-                </Dropdown.Toggle>
+              </Dropdown.Toggle>
 
-                <Dropdown.Menu className="nav-avatar-menu" alignRight>
-                  <div className="nav-avatar-container mb-3">
-                    <div className="nav-avatar-item">Pauras Swami</div>
-                    <div className="nav-avatar-item">pauras53@yahoo.com</div>
-                  </div>
-                  <Dropdown.Item href="">
-                    <i class="fas fa-cog mr-2" />
-                    My Profile
-                  </Dropdown.Item>
-                  <Dropdown.Item href="">
-                    <i class="fas fa-sign-out-alt mr-2" />
-                    Logout
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </Nav>
-
-          </Navbar.Collapse>
-        </Container>
+              <Dropdown.Menu className="nav-avatar-menu" alignRight>
+                <div className="nav-avatar-container mb-3">
+                  <div className="nav-avatar-item">Pauras Swami</div>
+                  <div className="nav-avatar-item">pauras53@yahoo.com</div>
+                </div>
+                <Dropdown.Item href="">
+                  <i class="fas fa-cog mr-2" />
+                  My Profile
+                </Dropdown.Item>
+                <Dropdown.Item href="">
+                  <i class="fas fa-sign-out-alt mr-2" />
+                  Logout
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
