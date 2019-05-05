1af0b4d4dc10485281404fa6ca7b74bf00951d8e
Pauras Swami <pauras53@yahoo.com>
2019-05-04 22:02:23 -0700

    feat: load square appt

--- src/routes/BookOnline/BookOnline.jsx
+++ src/routes/BookOnline/BookOnline.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Container, Col, Row, Card } from 'react-bootstrap';
 
 import i18n from '../../i18n';
 import AppoinmentScheduler from '../../components/AppoinmentScheduler';
 
-const BookOnline = () => {
-  return (
-    <Container className="page-container">
-      <Row className="pt-5">
-        <Col>
-          <h1 className="page-header">
-            <div>{i18n.book_appointment}</div>
-          </h1>
-        </Col>
-      </Row>
-      <Row className="pt-5">
-        <Col>
-          <AppoinmentScheduler />
-        </Col>
-      </Row>
-    </Container>
-  );
+var loadScript = function(src) {
+  const tag = document.createElement('script');
+  const body = document.getElementById('square-scheduler');
+
+  tag.async = false;
+  tag.src = src;
+  body.innerHTML = '';
+  body.appendChild(tag);
+}
+
+class BookOnline extends React.Component {
+  componentDidMount() {
+    loadScript('https://square.site/appointments/buyer/widget/8b66a829-b7cb-44c3-aacd-e3c6def2a4af/G9MRFZPF8VR2W.js')
+  }
+
+  render() {
+    return (
+      <Container className="page-container">
+        <Row className="pt-5">
+          <Col>
+            <h1 className="page-header">
+              <div>{i18n.book_appointment}</div>
+            </h1>
+          </Col>
+        </Row>
+        <Row className="pt-5">
+          <Col>
+          <Card border="success">
+              <Card.Body id="square-scheduler">
+              <div className="spinner-border" role="status">
+                <span className="sr-only">Loading...</span>
+              </div>
+              </Card.Body>
+            </Card>
+          </Col>
+          {/* <Col>
+            <AppoinmentScheduler />
+          </Col> */}
+        </Row>
+      </Container>
+    );
+  }
 };
 
 
